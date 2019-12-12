7aa88ff3d9d9cf7c1d7f1b4796de2dfcedf73ced
Mark Berry <markberry867@gmail.com>
2019-12-11 18:00:41 -0600

    Partially fix scripts for windows build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,10 @@ rm -rf dist/packages/builders
 
 tsc -p packages/builders/tsconfig.json
 
-rsync -a packages/builders/. dist/packages/builders --exclude *.ts --exclude tsconfig.json
+# rsync -a packages/builders/. dist/packages/builders --exclude *.ts --exclude tsconfig.json
+cp packages/builders/src/build-ng-packagr/src/build/schema.json dist/packages/builders/src/build-ng-packagr/src/build/schema.json
+cp packages/builders/src/build-ng-packagr/builders.json dist/packages/builders/src/build-ng-packagr/builders.json
+cp packages/builders/src/build-ng-packagr/package.json dist/packages/builders/src/build-ng-packagr/package.json
 
 cp LICENSE dist/packages/builders/src/build-ng-packagr/LICENSE
 cp README.md dist/packages/builders/src/build-ng-packagr/README.md
